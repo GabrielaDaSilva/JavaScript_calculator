8432fc6542e7959cf1c5ed791162e73e12417ffa
GabrielaDaSilva <111237935+GabrielaDaSilva@users.noreply.github.com>
2022-09-10 17:21:18 +0200

    JavaScript.update.4

--- javascript.js
+++ javascript.js
@@ -24,8 +24,8 @@ function multiply(num1, num2) {
 
 function divide(num1, num2) {
   let dev = num1 / num2;
-  if (num1 === "" && num2 === 0) {
-    alert('0')
+  if (num2 === 0) {
+    return "Error";
   } else {
     return dev;
   }
