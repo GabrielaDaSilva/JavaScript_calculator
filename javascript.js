052123a72fc331847308c99e716f6aa26494b6f3
GabrielaDaSilva <111237935+GabrielaDaSilva@users.noreply.github.com>
2022-09-12 20:55:32 +0200

    Update Javascript.6

--- javascript.js
+++ javascript.js
@@ -1,138 +1,164 @@
-const numberBut = document.querySelectorAll('.number');
-const operationBut = document.querySelectorAll('.operation');
-const previousOperand = document.querySelector('.previousOperand');
-const currentOperand = document.querySelector('.currentOperand');
-currentOperand.textContent = ' ';
-previousOperand.textContent = ' ';
-//The textContent property sets or returns the text content of the specified node, and all its descendants.
+let firstNumber = "";
+let secondNumber = "";
+let operator = "";
 
+const currentOperand = document.querySelector(".currentOperand");
+const previousOperand = document.querySelector(".previousOperand");
 
-function addition(num1, num2) {
-  let add = num1 + num2;
-  return add;
-}
-
-function subtraction(num1, num2) {
-  let sub = num1 - num2;
-  return sub;
-}
-
-function multiply(num1, num2) {
-  let mul = num1 * num2;
-  return mul;
-}
-
-function divide(num1, num2) {
-  let dev = num1 / num2;
-  if (num2 === 0) {
-    return "Error";
-  } else {
-    return dev;
+/* Adding event listeners for buttons*/
+const equal = document.querySelector("#equal");
+equal.addEventListener("click", () => {
+  if (secondNumber != "" && firstNumber != "") {
+    compute();
   }
-}
-
-// Create a new function operate that takes an operator and 2 numbers and then calls one of the above functions on the numbers.
-function operate(num1, num2, operator) {
-  switch (operator) {
-    case "+":
-      return addition(num1, num2);
-    case "-":
-      return subtraction(num1, num2);
-    case "*":
-      return multiply(num1, num2);
-    case "/":
-      return divide(num1, num2);
-  }
-};
-
-function handleNumber(valueNumber) {
-  console.log(valueNumber)
-}
-
-numberBut.forEach((btn) => {
-  btn.addEventListener("click", (e) => {
-    handleNumber(e.target.value);
-  });
 });
-
-let storedValue = '';
-let firstNumber = '';
-let clickedOperator = '';
-let result = '';
-currentOperand.textContent = 0;
-
-numberBut.forEach((number) => {
-  number.addEventListener('click', function () {
-    storedValue = number.value;
-    previousOperand.textContent += storedValue;
-  });
-});
-
-operationBut.forEach((operator => {
-  operator.addEventListener('click', function () {
-    // save the first number
-    firstNumber = storedValue;
-    // get the operator that was clicked
-    clickedOperator = operator.textContent;
-    previousOperand.textContent += clickedOperator;
-    storedValue = '';
-    console.log(clickedOperator);
-  })
-}));
-/*
-The parseFloat() function is used to accept the string and convert it into a floating-point number. 
-If the string does not contain a numeral value or If the first character of the string is not a Number then it returns NaN i.e, not a number.*/
-const calculate = () => {
-  const result = operate(parseFloat(firstNumber), parseFloat(storedValue), clickedOperator);
-  currentOperand.textContent = result;
-}
-
-const equalsBut = document.querySelector('#equals');
-equalsBut.addEventListener('click', function () {
-  calculate();
-  console.log("is equal to " + currentOperand.textContent);
-});
-
-const allClearbut = document.querySelector('#allClear');
-allClearbut.addEventListener('click', () => {
-  previousOperand.textContent = "";
-  currentOperand.textContent = "";
-});
-
-const deleteBut = document.querySelector('.delete');
-deleteBut.addEventListener('click', () => {
-  let currentString = previousOperand.textContent;
-  previousOperand.textContent = currentString.substring(0, currentString.length - 1);
-})
 
 const decimal = document.querySelector(".decimal");
 decimal.addEventListener("click", () => {
   addDecimal();
-  decimal.disabled == true;
 });
-function addDecimal() {
-  if (previousOperand.textContent.includes(".") == false) {
-    previousOperand.textContent += ".";
-    return parseFloat(storedValue, 10);
+
+const clear = document.querySelector("#allClear");
+clear.addEventListener("click", clearCalculator);
+
+const numberButtons = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
+
+numberButtons.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    handleNumber(e.target.textContent);
+  });
+});
+
+/*this prohibits numbers from goinf off the scfeen. Limits it to 9 numbers.*/
+function handleNumber(number) {
+  if (firstNumber !== "" && secondNumber !== "" && operator === "") {
+    firstNumber = "";
+    currentOperand.textContent = secondNumber;
+  }
+  if (secondNumber.length <= 9) {
+    secondNumber += number;
+    currentOperand.textContent = secondNumber;
   }
 }
-const zeroButton = document.querySelector("#zero");
-zeroButton.addEventListener("click", () => {
- onlyOneZero();
- addZero.disabled == true;
-})
-function onlyOneZero(){
-  if (previousOperand.textContent.includes("0") === false) {
-    previousOperand.textContent += "0";
- }
-}
 
-/*const percentageBut = document.querySelector(".percentage")
-percentageBut.addEventListener("click", () => {
-  previousOperand.textContent = (storedValue / 100);
+operators.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    handleOperator(e.target.textContent);
+  });
 });
 
-const signSwap = document.querySelector("#signSwap")
-signSwap.addEventListener("click", () => {
-  previousOperand.textContent = (storedValue * -1);
-});*/
+/* This is a function to handel how the operators function. */
+function handleOperator(op) {
+  if (firstNumber === "") {
+    firstNumber = secondNumber;
+    operatorCheck(op);
+  } else if (secondNumber === "") {
+    operatorCheck(op);
+  } else {
+    compute();
+    operator = op;
+    currentOperand.textContent = "0";
+    previousOperand.textContent = firstNumber + " " + operator;
+  }
+}
+
+function operatorCheck(text) {
+  operator = text;
+  previousOperand.textContent = firstNumber + " " + operator;
+  currentOperand.textContent = "0";
+  secondNumber = "";
+}
+
+function compute() {
+  firstNumber = Number(firstNumber);
+  secondNumber = Number(secondNumber);
+
+  if (operator === "+") {
+    firstNumber += secondNumber;
+  } else if (operator === "-") {
+    firstNumber -= secondNumber;
+  } else if (operator === "x") {
+    firstNumber *= secondNumber;
+  } else if (operator === "/") {
+    if (secondNumber <= 0) {
+      firstNumber = "Error";
+      displayResults();
+      return;
+    }
+    firstNumber /= secondNumber;
+  }
+  firstNumber = roundNumber(firstNumber);
+  firstNumber = firstNumber.toString();
+  displayResults();
+}
+
+function roundNumber(num) {
+  return Math.round(num * 10000) / 10000;
+}
+
+function displayResults() {
+  if (firstNumber.length <= 9) {
+    currentOperand.textContent = firstNumber;
+  } else {
+    currentOperand.textContent = firstNumber.slice(0, 9) + "...";
+  }
+  previousOperand.textContent = "";
+  operator = "";
+  secondNumber = "";
+}
+
+function clearCalculator() {
+  secondNumber = "";
+  firstNumber = "";
+  operator = "";
+  currentOperand.textContent = "0";
+  previousOperand.textContent = "";
+}
+
+function addDecimal() {
+  if (!secondNumber.includes(".")) {
+    secondNumber += ".";
+    currentOperand.textContent = secondNumber;
+  }
+}
+
+function handleKeyPress(e) {
+  e.preventDefault();
+  if (e.key >= 0 && e.key <= 9) {
+    handleNumber(e.key);
+  }
+  if (
+    e.key === "Enter" ||
+    (e.key === "=" && secondNumber != "" && firstNumber != "")
+  ) {
+    compute();
+  }
+  if (e.key === "+" || e.key === "-" || e.key === "/") {
+    handleOperator(e.key);
+  }
+  if (e.key === "*") {
+    handleOperator("x");
+  }
+  if (e.key === ".") {
+    addDecimal();
+  }
+  if (e.key === "Backspace") {
+    handleDelete();
+  }
+}
+
+const deleteBut = document.querySelector('.delete');
+deleteBut.addEventListener('click', () => {
+  if (secondNumber !== "") {
+    secondNumber = secondNumber.slice(0, -1);
+    currentOperand.textContent = secondNumber;
+    if (secondNumber === "") {
+      currentOperand.textContent = "0";
+    }
+  }
+  if (secondNumber === "" && firstNumber !== "" && operator === "") {
+    firstNumber = firstNumber.slice(0, -1);
+    currentOperand.textContent = firstNumber;
+  }
+});
